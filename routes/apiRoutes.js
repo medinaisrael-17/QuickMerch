2a91b3889e93e42009f6f88401475e337ab0d33d
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-03-02 14:19:35 -0600

    working on auth

---
+++ routes/apiRoutes.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+const passport = require("../config/passport");
+
+module.exports = function (app) {
+    app.post("/api/login", passport.authenticate("local"), function (req, res) {
+        res.json({
+            email: req.user.email,
+            id: req.user.id
+        });
+    });
+
+    app.post("/api/signup", function (req, res) {
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(function () {
+                res.redirect(307, "/api/login")
+            })
+            .catch(function (err) {
+                res.status(401).json(err)
+            });
+    });
+
+    app.get("/api/user_data", function (req, res) {
+        if (!req.user) {
+            res.json({});
+        }
+        else {
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
+};
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-var express = require("express");
-var mongoose = require("mongoose");
+const express = require("express");
+const session = require("express-session");
 
-var app = express();
-var PORT = process.env.PORT || 3001;
+// passport
+const passport = require("./config/passport")
 
-app.use(express.static("public"));
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+const app = express();
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/quickmerch", {
-  useNewUrlParser: true
-});
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
-app.listen(PORT, function() {
-  console.log(`Now listening on port: ${PORT}`);
-});
+db.sequilize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  })
+})
