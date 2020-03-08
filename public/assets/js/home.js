a8a88d13beb13d4b9b396bcde58ba24942ddbedf
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-03-08 16:04:00 -0500

    working on generating cards per user

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-    let user_id;
     try {
         //display current date
         $("#date").text(moment().format("dddd, MMMM Do"));
@@ -17,19 +15,88 @@ $(document).ready(function () {
         return;
     }
 
+    const routePreviewDiv = $("#routePreview")
+
 
     $.get("/api/user_data").then(function (data) {
         console.log(data);
         $("#username").text(data.name);
-        user_id = data.id
+        const user_id = data.id
         console.log(user_id);
-        const userObj = {
-            id: user_id
-        }
-        $.get("/api/user_routes", userObj).then(function (res) {
+
+        $.get(`/api/user_routes/${user_id}`).then(function (res) {
             console.log(res);
+            const routeData = res.Routes;
+            loadCards(routeData);
         })
     })
+
+    function loadCards(data) {
+        console.log(data);
+
+        if (data.length === 0) {
+            routePreviewDiv.html("");
+            let h1 = $(`<h1>No Routes Assigned</h1>`);
+            routePreviewDiv.append(h1)
+        }
+        else {
+            routePreviewDiv.html("");
+            for (let i = 0; i < 3; i++) {
+                if (data[i].completed === true) {
+                    console.log("skip");
+                }
+
+                else {
+
+                    if (data[i].store === "H-E-B") {
+                        let img = $(`
+                       <div class="myOverviewCard">
+                       <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0002/3066/brand.gif?itok=lhacHCP3"
+                           class="card-img-top" alt="store">
+                       <div class="smallContent">
+                           <h5>${data[i].location}</h5>
+                           <p class="smallText"><em><span class="time">${data[i].time}</span></em></p>
+                           <a href="/routes">Click for more...</a>
+                       </div>
+                   </div>
+                       `);
+                        routePreviewDiv.append(img)
+                    }
+                    else if (data[i].store === "Walmart") {
+                        let img = $(`
+                       <div class="myOverviewCard">
+                       <img src="https://www.usaddress.com/blog/wp-content/uploads/2019/06/Walmart-Logo.jpg"
+                           class="card-img-top" alt="store">
+                       <div class="smallContent">
+                           <h5>${data[i].location}</h5>
+                           <p class="smallText"><em><span class="time">${data[i].time}</span></em></p>
+                           <a href="/routes">Click for more...</a>
+                       </div>
+                   </div>
+                       `);
+                        routePreviewDiv.append(img);
+                    }
+                    else if (data[i].store === "Randalls") {
+                        let img = $(`
+                    <div class="myOverviewCard">
+                    <img src="https://dynl.mktgcdn.com/p/UsjblGpF7V89qZ3q-qig9uyGjopHRek0GQqhTC05mGk/1746x1746.png"
+                        class="card-img-top" alt="store">
+                    <div class="smallContent">
+                        <h5>${data[i].location}</h5>
+                        <p class="smallText"><em><span class="time">${data[i].time}</span></em></p>
+                        <a href="/routes">Click for more...</a>
+                    </div>
+                </div>
+                    `)
+                        routePreviewDiv.append(img);
+                    }
+                }
+
+            }
+        }
+
+    }
+
 
 
 })
