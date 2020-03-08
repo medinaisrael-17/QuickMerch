48666924c2b590a0b5d82c987b58d8aebd274a60
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-03-08 16:16:32 -0500

    proper

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false,
         },
         lastName: {
-            type: DataTypes.String,
+            type: DataTypes.STRING,
             allowNull: false
         },
         phoneNumber: {
