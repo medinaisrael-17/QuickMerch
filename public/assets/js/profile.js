f3dbd090e5cf12b1bd994fbd0e88319cc7a993db
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-03-09 10:50:01 -0500

    updated the profile page to accuratley show first and last name

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $.get("/api/user_data").then(function (data) {
         console.log(data);
-        $("#username").text(data.name);
+        $("#username").text(`${data.firstName} ${data.lastName}`);
         $("#contact").text(data.phoneNumber);
         $("#userEmail").text(data.email);
     })
 
-    $.get("/api")
+    $.get("/api");
 })
 
 
