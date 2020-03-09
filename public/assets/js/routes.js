37e93721249a036baa59bdfd95af60390332622d
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-03-09 15:07:21 -0500

    making a distinction between completed and incomplete routes

--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     const overViewDiv = $("#overview");
 
+    let routeData;
+
     $.get("/api/user_data").then(function (data) {
         console.log(data);
         $("#username").text(data.name);
@@ -10,31 +12,73 @@ $(document).ready(function () {
 
         $.get(`/api/user_routes/${user_id}`).then(function (res) {
             console.log(res);
-            const routeData = res.Routes;
-            loadCards(routeData);
+            routeData = res.Routes;
+            $("#myButton").text("Show Completed Routes");
+
+            if (routeData.length === 0) {
+                overViewDiv.html("");
+                const h1 = $(`<h1>No Routes Assigned</h1>`);
+                overViewDiv.append(h1)
+            }
+
+            loadIncompleted(routeData);
         })
     })
 
-    function loadCards(data) {
-        console.log(data);
-
-        if (data.length === 0) {
-            overViewDiv.html("");
-            const h1 = $(`<h1>No Routes Assigned</h1>`);
-            overViewDiv.append(h1)
+    $("#myButton").click(function () {
+        const status = $(this).attr("status");
+        console.log("clicked");
+        if (status === "incomplete") {
+            $(this).attr("status", "complete");
+            $(this).text("Show Incomplete Routes");
+            loadCompleted(routeData);
         }
         else {
-            overViewDiv.html("");
+            $(this).attr("status", "incomplete")
+            $(this).text("Show Completed Routes");
+            loadIncompleted(routeData);
+        }
 
-            const heading = $(`
-            <h4>All routes</h4>
+    });
+
+    $(document).on("click", ".complete", function () {
+        const id = $(this).attr("id");
+        $.put(`/api/routes/${id}`, {status: true}).then(loadIncompleted(routeData));
+    })
+
+    function loadCompleted(data) {
+        const completedData = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].completed) {
+                completedData.push(data[i]);
+            }
+        }
+        loadCards(completedData, "Completed");
+    }
+
+    function loadIncompleted(data) {
+        const incompleteData = [];
+        for (let i = 0; i < data.length; i++) {
+            if (!data[i].completed) {
+                incompleteData.push(data[i]);
+            }
+        };
+        loadCards(incompleteData, "Incomplete");
+    }
+
+    function loadCards(data, status) {
+        // console.log(data);
+        overViewDiv.html("");
+
+        const heading = $(`
+            <h4>${status} Routes</h4>
             <hr>
             `)
 
-            overViewDiv.append(heading);
-            for (let i = 0; i < 3; i++) {
-                if (data[i].store === "H-E-B") {
-                    const card = $(`
+        overViewDiv.append(heading);
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].store === "H-E-B") {
+                const card = $(`
                     <div class="card">
             <img class="storeImage"
                 src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0002/3066/brand.gif?itok=lhacHCP3">
@@ -43,14 +87,14 @@ $(document).ready(function () {
                 <hr>
                 <p class="arrive"><strong>Arrive at: </strong> <em><span class="time">${data[i].time}</span></em></p>
                 <p class="product"><strong>Product:</strong> <em><span class="toDo">${data[i].product}</span></em> </p>
-                ${data[i].completed ? "" : "<button class='btn btn-success complete'>COMPLETE</button>"}
+                ${data[i].completed ? `<button class='btn btn-secondary complete'id=${data[i].id}>UNDO</button>` : `<button class='btn btn-success complete'id=${data[i].id}>COMPLETE</button>`}
             </div>
         </div>
                     `)
-                    overViewDiv.append(card)
-                }
-                else if (data[i].store === "Walmart") {
-                    const card = $(`
+                overViewDiv.append(card)
+            }
+            else if (data[i].store === "Walmart") {
+                const card = $(`
                     <div class="card">
             <img class="storeImage"
                 src="https://www.usaddress.com/blog/wp-content/uploads/2019/06/Walmart-Logo.jpg">
@@ -59,14 +103,14 @@ $(document).ready(function () {
                 <hr>
                 <p class="arrive"><strong>Arrive at: </strong> <em><span class="time">${data[i].time}</span></em></p>
                 <p class="product"><strong>Product:</strong> <em><span class="toDo">${data[i].product}</span></em> </p>
-                <button class="btn btn-success complete">COMPLETE</button>  
+                ${data[i].completed ? `<button class='btn btn-secondary complete'id=${data[i].id}>UNDO</button>` : `<button class='btn btn-success complete'id=${data[i].id}>COMPLETE</button>`}
             </div>
         </div>
                     `)
-                    overViewDiv.append(card);
-                }
-                else if (data[i].store === "Randalls") {
-                    const card = $(`
+                overViewDiv.append(card);
+            }
+            else if (data[i].store === "Randalls") {
+                const card = $(`
                     <div class="card">
             <img class="storeImage"
                 src="https://dynl.mktgcdn.com/p/UsjblGpF7V89qZ3q-qig9uyGjopHRek0GQqhTC05mGk/1746x1746.png">
@@ -75,14 +119,14 @@ $(document).ready(function () {
                 <hr>
                 <p class="arrive"><strong>Arrive at: </strong> <em><span class="time">${data[i].time}</span></em></p>
                 <p class="product"><strong>Product:</strong> <em><span class="toDo">${data[i].product}</span></em> </p>
-                <button class="btn btn-success complete">COMPLETE</button>  
+                ${data[i].completed ? `<button class='btn btn-secondary complete'id=${data[i].id}>UNDO</button>` : `<button class='btn btn-success complete'id=${data[i].id}>COMPLETE</button>`}  
             </div>
         </div>
                     `)
-                    overViewDiv.append(card);
-                }
+                overViewDiv.append(card);
             }
         }
+
 
     }
 })
--- routes/route-api-routes.js
+++ routes/route-api-routes.js
@@ -10,12 +10,14 @@ module.exports = function (app) {
     //     db.
     // })
 
+    //create the routes
     app.post("/api/routes", function (req, res) {
         db.Route.create(req.body).then(function (dbRoute) {
             res.json(dbRoute);
         });
     });
 
+    //delete the routes
     app.delete("/api/routes/:routeId", function (req, res) {
         db.Route.destroy({
             where: {
@@ -26,29 +28,34 @@ module.exports = function (app) {
         });
     });
 
+    //update who the route is assigned to 
     app.put("/api/routes/", function (req, res) {
+        console.log(req.body.id);
         db.Route.update(
-            {isAssigned: true},
-            { UserId: req.body.user_id },
             {
-                where: {
-                    id: req.body.id
-                }
-            })
+                isAssigned: true,
+                UserId: req.body.user_id
+            },
+            { where: { id: req.body.id } }
+        )
             .then(function (dbRoute) {
                 res.json(dbRoute);
             });
     });
 
-    app.put("/api/routes/:routeId", function(req, res) {
+    //update the routes completion 
+    app.put("/api/routes/:routeId", function (req, res) {
+        console.log(req.body)
         db.Route.update({
-            completed: true
+            completed: req.body.status
         },
-        {where: {
-            id: req.params.routeId
-        }})
-        .then(function(dbRoute) {
-            res.json({dbRoute});
-        });
+            {
+                where: {
+                    id: req.params.routeId
+                }
+            })
+            .then(function (dbRoute) {
+                res.json({ dbRoute });
+            });
     });
 }
