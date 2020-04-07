ddfb4382f7286582dddbdb03928a2a72eaa78b52
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-04-07 13:40:35 -0500

    trying to see how to access local storage on mobile

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,53 +1,27 @@
 $(document).ready(function () {
     console.log(navigator.userAgent);
-    function getLocation() {
-        // Make sure browser supports this feature
-        if (navigator.geolocation) {
-          // Provide our showPosition() function to getCurrentPosition
-          navigator.geolocation.getCurrentPosition(showPosition);
-        } 
-        else {
-          alert("Geolocation is not supported by this browser.");
-        }
-      }
 
-      // This will get called after getCurrentPosition()
-      function showPosition(position) {
-        // Grab coordinates from the given object
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        console.log("Your coordinates are Latitude: " + lat + " Longitude " + lon);
+    const visited = localStorage.getItem("visited");
 
-        // Call our next function, passing on the coordinates
-        redirect(lat, lon);
-      }
+    if (!visited) {
+        alert("You haven't been here yet!");
+        localStorage.setItem("visited", true);
+    }
 
-      function redirect(lat, lon) {
-        // Compare latitude and longitude values
-        if (lat >= 30 && lon <= -100) {
-          // Redirect to a new page
-        //   window.location.href = "western.html";
-        } 
-        else {
-        //   window.location.href = "eastern.html";
-        }
-      }
-
-      // See if user is on Windows
-      if (navigator.userAgent.indexOf("Android") !== -1) {
+    // See if user is on Windows
+    if (navigator.userAgent.indexOf("Android") !== -1) {
         alert("Welcome, Android user!");
-        getLocation();
-      } 
-      // Then check for macOS
-      else if (navigator.userAgent.indexOf("iPhone") !== -1) {
+    }
+    // Then check for macOS
+    else if (navigator.userAgent.indexOf("iPhone") !== -1) {
         alert("Welcome, iPhone user!");
-        getLocation();
-      }
-      // I give up
-      else {
+    }
+    // I give up
+    else {
         alert("I don't know what you're using...");
-      }
-       
+    }
+
+
     const signUpForm = $("form.signup");
     const emailInput = $("input#email-input");
     const firstNameInput = $("input#firstName-input");
@@ -66,7 +40,7 @@ $(document).ready(function () {
         };
         console.log(userData);
 
-        if (!userData.email || !userData.firstName || !userData.lastName ||  !userData.phoneNumber || !userData.password) {
+        if (!userData.email || !userData.firstName || !userData.lastName || !userData.phoneNumber || !userData.password) {
             return;
         }
 
@@ -97,13 +71,13 @@ $(document).ready(function () {
         $("#alert").fadeIn(500);
     }
 
-    $(document).on("click", ".fa-eye", function() {
+    $(document).on("click", ".fa-eye", function () {
         $("#password-input").attr("type", "text");
         $("#eye").removeClass("fa fa-eye");
         $("#eye").addClass("fa-eye-slash");
     })
 
-    $(document).on("click", ".fa-eye-slash", function() {
+    $(document).on("click", ".fa-eye-slash", function () {
         $("#password-input").attr("type", "password");
         $("#eye").removeClass("fa fa-eye-slash");
         $("#eye").addClass("fa fa-eye");
