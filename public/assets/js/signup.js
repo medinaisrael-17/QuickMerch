b44aaadada08f692eb3987bbbac081193d6e6aea
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-04-07 13:25:36 -0500

    finding out which device they are on

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,4 +1,53 @@
 $(document).ready(function () {
+    console.log(navigator.userAgent);
+    function getLocation() {
+        // Make sure browser supports this feature
+        if (navigator.geolocation) {
+          // Provide our showPosition() function to getCurrentPosition
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } 
+        else {
+          alert("Geolocation is not supported by this browser.");
+        }
+      }
+
+      // This will get called after getCurrentPosition()
+      function showPosition(position) {
+        // Grab coordinates from the given object
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        console.log("Your coordinates are Latitude: " + lat + " Longitude " + lon);
+
+        // Call our next function, passing on the coordinates
+        redirect(lat, lon);
+      }
+
+      function redirect(lat, lon) {
+        // Compare latitude and longitude values
+        if (lat >= 30 && lon <= -100) {
+          // Redirect to a new page
+        //   window.location.href = "western.html";
+        } 
+        else {
+        //   window.location.href = "eastern.html";
+        }
+      }
+
+      // See if user is on Windows
+      if (navigator.userAgent.indexOf("Android") !== -1) {
+        alert("Welcome, Android user!");
+        getLocation();
+      } 
+      // Then check for macOS
+      else if (navigator.userAgent.indexOf("iPhone") !== -1) {
+        alert("Welcome, iPhone user!");
+        getLocation();
+      }
+      // I give up
+      else {
+        alert("I don't know what you're using...");
+      }
+       
     const signUpForm = $("form.signup");
     const emailInput = $("input#email-input");
     const firstNameInput = $("input#firstName-input");
