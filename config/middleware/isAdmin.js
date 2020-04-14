70a3ef7d88cbcca337aa8a2a974f33e594f087ac
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-04-14 12:14:21 -0500

    adding update capabilities

--- config/middleware/isAdmin.js
+++ config/middleware/isAdmin.js
@@ -1,11 +1,12 @@
 module.exports = function (req, res, next) {
+    if (req.user && !req.user.isAdmin) {
+        res.redirect("/home");
+    }
+
     if (req.user && req.user.isAdmin) {
         return next();
     }
 
-    if (req.user && !req.user.isAdmin) {
-        res.redirect("/home");
-    }
 
     return res.redirect("/");
 }
