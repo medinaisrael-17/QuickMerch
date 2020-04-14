6133a8540985dbf0afac31004d9c0e62cf48e729
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2020-04-14 13:27:54 -0500

    taking away test cases

--- public/service-worker.js
+++ public/service-worker.js
@@ -33,7 +33,7 @@ const FILES_TO_CACHE = [
     '/assets/images/icons/icon-512x512.png',
 ];
 
-const CACHE_NAME = "static-cache-v1.0.03";
+const CACHE_NAME = "static-cache-v1.0.04";
 const DATA_CACHE_NAME = "data-cache-v1"
 
 //install 
